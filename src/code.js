418826720dce9cf720a7869e770a1f3013f6ceda
gloomysergei <almazopal@gmail.com>
2021-03-03 20:15:55 +0300

    step_2 - adding arguments and an option `format`

--- src/code.js
+++ src/code.js
@@ -5,7 +5,9 @@ const commander = new Command();
 function finddiff() {
   commander
     .version('1.0.0')
-    .description('Compares two configuration files and shows a difference.');
+    .description('Compares two configuration files and shows a difference.')
+    .arguments('<filepath1>, <filepath2>')
+    .option('-f, --format [type]', 'output format');
   commander.parse(process.argv);
 }
 export default finddiff;
