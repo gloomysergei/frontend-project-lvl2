1ef57e2283e03179b7103604339faec50547811d
gloomysergei <almazopal@gmail.com>
2021-06-01 18:49:18 +0300

    return to the initial value of step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const commander = new Command();
 commander
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstFile>', '<secondFile>')
+  .arguments('<firstFile> <secondFile>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action((firstFile, secondFile, options) => {
     const diff = finddiff(firstFile, secondFile, options.format);
