c67823cead20c5e39e4fd2b3ae216ad83cf426b7
gloomysergei <almazopal@gmail.com>
2021-06-17 20:33:37 +0300

    Hexlet tests-step 1- change fileReader

--- src/fileReader.js
+++ src/fileReader.js
@@ -4,11 +4,8 @@ import { dirname, join, extname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-
 const getFormat = (filename) => extname(filename).substring(1);
-
 const getFileContents = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 export { getFixturePath, getFormat, getFileContents };
